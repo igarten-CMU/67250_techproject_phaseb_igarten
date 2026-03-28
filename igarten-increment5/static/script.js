5634a0817c802b325d9c259fc2066fcbc369857e
igarten-CMU <igarten@andrew.cmu.edu>
2026-03-28 16:03:59 -0400

    increment 5 (2nd draft)

--- igarten-increment5/static/script.js
+++ igarten-increment5/static/script.js
@@ -171,3 +171,19 @@ function toggleMenu() {
     var nav = document.querySelector(".nav_bar");
     nav.classList.toggle("responsive");
 }
+
+function initMap() {
+    if(window.location.href != "explore.html"){
+        return;
+    }
+    var map = L.map('map').setView([40.7128, -74.0060], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+    L.marker([40.7128, -74.0060]).addTo(map)
+        .bindPopup("MonoMuse")
+        .openPopup();
+}
+
+initMap();
